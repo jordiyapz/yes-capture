8f8927345f4e69223c3ad3df0cfbd298d1b12fef
Jordi Yaputra <jordiyaputra@gmail.com>
2022-01-12 01:15:52 +0700

    refactor: notebook 1 & 2

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='1.0.0',
     author='Jordi Yaputra',
     packages=['yesc'],
-    install_requires=['tensorflow', 'pandas',
-                      'matplotlib', 'opencv-python', 'mediapipe'],
+    install_requires=['tensorflow', 'pandas', 'matplotlib', 'opencv-python',
+                      'mediapipe', 'python-dotenv', 'git+https://github.com/jordiyapz/myutils-python'],
 )
---
+++ yesc/utils/mediapipe.py
@@ -0,0 +1,5 @@
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
